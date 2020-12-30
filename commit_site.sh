9c89a4f79f9da2a6f6c96113f5082fcb8b088232
Tyler Benster <tbenst@gmail.com>
2020-12-29 21:48:19 -0800

    github actions cachix test

--- commit_site.sh
+++ commit_site.sh
@@ -26,8 +26,8 @@ fi
 mkdir site
 cd site
 # now lets setup a new repo so we can update the site branch
-git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
-git config --global user.name "$GH_NAME" > /dev/null 2>&1
+git config --global user.email "action@github.com" > /dev/null 2>&1
+git config --global user.name "GitHub Action" > /dev/null 2>&1
 git init
 git remote add --fetch origin "$remote"
 
@@ -49,15 +49,3 @@ cp -a "../${siteSource}/." .
 git add -A
 # now commit, ignoring branch site doesn't seem to work, so trying skip
 git commit --allow-empty -m "Deploy to GitHub branch site [ci skip]"
-
-# Note: disable push now that we use Github Actions
-# and push, but send any output to /dev/null to hide anything sensitive
-# git push --force --quiet origin site
-# # git push --force --quiet origin site > /dev/null 2>&1
-
-# # go back to where we started and remove the site git repo we made and used
-# # for deployment
-# cd ..
-# rm -rf site
-
-# echo "Finished Deployment!"
