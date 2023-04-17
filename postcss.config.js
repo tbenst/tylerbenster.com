4d23399cf8fa2695570e0b96e33db543c842c7f2
Tyler Benster <tbenst@gmail.com>
2023-04-16 20:30:09 -0700

    comment out css purge for now...

--- postcss.config.js
+++ postcss.config.js
@@ -3,13 +3,13 @@ module.exports = {
     require('postcss-import'),
     require('tailwindcss'),
     require('postcss-100vh-fix'),
-    require('@fullhuman/postcss-purgecss')({
-      content: [
-        './static/**/*.html',
-        './static/**/*.markdown',
-        './static/**/*.md',
-      ]
-    }),
+    // require('@fullhuman/postcss-purgecss')({
+    //   content: [
+    //     './static/**/*.html',
+    //     './static/**/*.markdown',
+    //     './static/**/*.md',
+    //   ]
+    // }),
     require('autoprefixer'),
   ]
 }
